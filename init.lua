3c885a1d2dcfcfd16386f7c7be657ed2eb5fdb3f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-10-30 14:19:36 +0900

    enable pyright

--- init.lua
+++ init.lua
@@ -632,7 +632,12 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {
+          enabled = true,
+        },
+        ruff_lsp = {
+          enabled = false,
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
